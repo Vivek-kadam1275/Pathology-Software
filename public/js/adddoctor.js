ca87dd71a5f981bd9c23e7f86c95f223c2cab7b9
Vivek-kadam1275 <142383041+Vivek-kadam1275@users.noreply.github.com>
2024-06-27 12:44:05 +0530

    Delete public directory

--- public/js/adddoctor.js
+++
@@ -1,4 +0,0 @@
-// const button=document.getElementById('submit');
-//   button.addEventListener('click',()=>{
-//        alert("Entered Successfully");
-//   })
--- public/js/report.js
+++
@@ -1,9 +0,0 @@
-const button=document.getElementById('btn');
-    const body=document.querySelector('body');
-     button.addEventListener('click',()=>{
-      button.classList.add('addButton');
-      body.classList.add('addBody')
-      window.print();
-      button.classList.add('addButton1');
-      body.classList.add('addBody1');
-    })
